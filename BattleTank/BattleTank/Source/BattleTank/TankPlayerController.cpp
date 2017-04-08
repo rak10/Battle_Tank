a857785fadf832233a8d53abc826b484068e427e
Asad Raja <asadkhalidraja@gmail.com>
2017-04-08 23:19:37 +0200

    BT26 Deprojected screen to world (aiming shows real coordinates of line trace)

--- BattleTank/BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -55,9 +55,20 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
 	int32 ViewportSizeX, ViewportSizeY;
 	GetViewportSize(ViewportSizeX, ViewportSizeY);
 	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
-	UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *ScreenLocation.ToString());
-
+	
 	//De-project the screen position of crosshair to a world direction
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *LookDirection.ToString());
+	}
 	//Line-trace along that look direction, and see what we hit (up to max range) 
 	return true;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CameraWorldLocation;	//To be discarded
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CameraWorldLocation, LookDirection);
+	
 }
--- BattleTank/BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/BattleTank/Source/BattleTank/TankPlayerController.h
@@ -31,5 +31,7 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	float CrossHairYLocation = 0.3333;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
 	
 };
