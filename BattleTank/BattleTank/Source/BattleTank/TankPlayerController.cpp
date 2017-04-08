ea7f20df45db13c5dac60ecdd2150317cd31ccfb
Asad Raja <asadkhalidraja@gmail.com>
2017-04-09 00:26:32 +0200

    BT27 Line traced aiming reticle

--- BattleTank/BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -41,7 +41,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation; //Out parameter
 	if (GetSightRayHitLocation(HitLocation))
 	{ 
-	//UE_LOG(LogTemp, Warning, TEXT("Look Location: %s"), *HitLocation.ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *HitLocation.ToString());
 
 	// TODO Tell controlled tank to aim at this point
 	}
@@ -60,11 +60,32 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
 	FVector LookDirection;
 	if (GetLookDirection(ScreenLocation, LookDirection))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *LookDirection.ToString());
+		//Line-trace along that look direction, and see what we hit (up to max range) 
+		GetLookVectorHitLocation(LookDirection, HitLocation);
 	}
-	//Line-trace along that look direction, and see what we hit (up to max range) 
+	
 	return true;
 }
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
+	if (GetWorld()->LineTraceSingleByChannel(
+				HitResult,
+				StartLocation,
+				EndLocation,
+				ECollisionChannel::ECC_Visibility)
+		)
+	{
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	HitLocation = FVector(0);
+	return false; //Line trace did not succeed
+}
+
 
 bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
 {
--- BattleTank/BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/BattleTank/Source/BattleTank/TankPlayerController.h
@@ -32,6 +32,11 @@ private:
 	UPROPERTY(EditAnywhere)
 	float CrossHairYLocation = 0.3333;
 
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000;
+
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 	
 };
