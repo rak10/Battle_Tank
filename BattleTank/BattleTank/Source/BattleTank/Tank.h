ae5ccf7c3b37d87bd13b9b4388914d0ce4cc40df
Asad Raja <asadkhalidraja@gmail.com>
2017-04-25 22:16:55 +0200

    BT29 Made a new TankAimComponent class and moved AimAt method from Tank.cpp

--- BattleTank/BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/BattleTank/Source/BattleTank/Tank.h
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h" //Put new includes above
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -16,7 +16,10 @@ public:
 	// Sets default values for this pawn's properties
 	ATank();
 
+	
 protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
